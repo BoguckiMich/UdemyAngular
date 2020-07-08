19cbef77fa8b50d2d6e9905f8cdbee9848a76f9d
BoguckiMich <bogucki.mich@gmail.com>
2020-07-08 14:54:28 +0200

    mongoose and first post in DB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const Post = require("./models/post");
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://pichael:UpH0Xsu1x1KSZjUv@cluster0.abvjm.mongodb.net/posts?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(() => {
+    console.log("connection to db failed");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,27 +33,20 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   res.status(201).json({ message: "blah" });
 });
 
 app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "dsad12r541",
-      title: "first server side post",
-      content: " this is comming from backend",
-    },
-    {
-      id: "asd124rtf",
-      title: "second server side post",
-      content: " this is also comming from backend",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched succesfully",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched succesfully",
+      posts: documents,
+    });
   });
 });
 
